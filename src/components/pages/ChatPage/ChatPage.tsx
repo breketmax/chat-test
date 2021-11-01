88c79e10d2deacfe8eaa49a3ccc2bdded2e87135
breketmax <maxim_228228228@mail.ru>
2021-11-02 00:06:51 +0700

    refactor: delete Fragments

--- src/components/pages/ChatPage/ChatPage.tsx
+++ src/components/pages/ChatPage/ChatPage.tsx
@@ -2,9 +2,5 @@ import React from "react";
 import { ChatPageTemplate } from "../../templates/ChatPageTemplate";
 
 export const ChatPage: React.FC = () => {
-  return (
-    <>
-      <ChatPageTemplate />
-    </>
-  );
+  return <ChatPageTemplate />;
 };
--- src/components/pages/LoginPage/LoginPage.tsx
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -2,9 +2,5 @@ import React from "react";
 import { LoginPageTemplate } from "../../templates/LoginPageTemplate";
 
 export const LoginPage: React.FC = () => {
-  return (
-    <>
-      <LoginPageTemplate />
-    </>
-  );
+  return <LoginPageTemplate />;
 };
